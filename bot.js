f2a96c91c54990e9c220ee78ab99f461fd34e150
HipWardX <51372882+HipWardX@users.noreply.github.com>
2019-06-05 21:32:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,13 +10,6 @@ client.on('message', message => {
     	message.reply('hello there');
   	}
 });
-});
-
-client.on('message', message => {
-    if (message.content === 'how are you') {
-    	message.reply('im great how about you');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
