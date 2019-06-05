ec5666b361ea062befb9655c89d06b08bcbff74d
HipWardX <51372882+HipWardX@users.noreply.github.com>
2019-06-05 21:07:16 +0700

    Update and rename bot.py to bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+    }
+ client.on('message', message => {
+    if (message.content === 'hi') {
+    	message.reply('hello there');
+    }
+client.on('message', message => {
+    if (message.content === 'how are you') {
+    	message.reply('im fine');
+    }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
